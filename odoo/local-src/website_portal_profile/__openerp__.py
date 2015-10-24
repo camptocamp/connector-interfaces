2b70010158fc4c433658346cc92a6c5b8f3c531e
Goran Sunjka <gs@sunjka.de>
2015-10-24 17:13:33 +0200

    Modul für Profil erstellt.

---
+++ odoo/local-src/website_portal_profile/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Website Portal Profile',
+    'author': 'Goran Sunjka',
+    'category': 'Website',
+    'summary': '',
+    'version': '1.0',
+    'description': """
+    Enhanced Portal Profile
+        """,
+    'website': 'https://www.sunjka.de/',
+    'depends': [
+        'website', 'website_portal'
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+}
