abbb6a306eed6324beeb3674e41149d21aea5455
Goran Sunjka <gs@sunjka.de>
2015-10-24 11:34:53 +0200

    Modul fluxdock_signup. Ansicht auth_signup angepasst.

---
+++ odoo/local-src/website_fluxdock_signup/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Website Fluxdock Signup',
+    'author': 'Goran Sunjka',
+    'category': 'Website',
+    'summary': '',
+    'version': '1.0',
+    'description': """
+    Fluxdock specific sign up
+        """,
+    'website': 'https://www.sunjka.de/',
+    'depends': [
+        'auth_signup', 'website', 'website_portal'
+    ],
+    'data': [
+        # 'views/templates.xml',
+    ],
+    'installable': True,
+}
---
+++ odoo/local-src/website_fluxdock_signup/controllers/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from openerp import http
+from openerp.http import request
+from openerp import tools
+from openerp.tools.translate import _
+
+from openerp.addons.website_portal.controllers.main import website_account
