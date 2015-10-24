3fda15504610961799014a8cfe5683261ab719f2
Goran Sunjka <gs@sunjka.de>
2015-10-24 18:42:01 +0200

    Model res_partner hinzugefügt.

---
+++ odoo/local-src/website_portal_profile/models/res_partner.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# from openerp.osv import orm, fields, osv
+from openerp import fields, models
+
+
+class ResPartner(models.Model):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+
+    werbeversprechen = fields.Char(string='Werbeversprechen')
+    facebook = fields.Char(string='Facebook')
+    twitter = fields.Char(string='Twitter')
+    expertise = fields.Char(string='Expertise')
