d37fbe587f766b084f9acf32e88b578982a9c94f
Goran Sunjka <gs@sunjka.de>
2015-10-28 12:11:23 +0100

    Profile controller.

--- odoo/local-src/website_portal_profile/controllers/main.py
+++ odoo/local-src/website_portal_profile/controllers/main.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+from openerp import http
+from openerp.http import request
+from openerp import tools
+from openerp.tools.translate import _
+
+from openerp.addons.website_portal.controllers.main import website_account
+
+class website_account(website_account):
+    @http.route(['/my/account'], type='http', auth="user", website=True)
+    def details(self, redirect=None, **post):
+        response = super(website_account, self).details()
+        categories = request.env['res.partner.category'].sudo().search([])
+        response.qcontext.update({
+            'categories': categories,
+        })
+        return response
