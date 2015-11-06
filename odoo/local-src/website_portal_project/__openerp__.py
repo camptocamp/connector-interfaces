459d1cc22655f6009509b70b9c0fb83a5e9eb63a
Goran Sunjka <gs@sunjka.de>
2015-11-06 19:05:51 +0100

    Modul website_portal_project.

---
+++ odoo/local-src/website_portal_project/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Website Portal Project',
+    'author': 'Goran Sunjka',
+    'website': 'http://www.sunjka.de',
+    'version': '0.1',
+    'category': 'Tools',
+    'complexity': 'easy',
+    'description': """
+This module adds projects inside your account's page on website.
+==================================================================================================
+    """,
+    'depends': ['project', 'website_portal'],
+    'data': [
+        'views/project_project_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'category': 'Hidden',
+}
