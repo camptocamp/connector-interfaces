90b35b041d09af91fec789669794c173877d5bb1
Goran Sunjka <gs@sunjka.de>
2015-11-07 10:34:54 +0100

    Project controllers.

--- odoo/local-src/website_portal_project/controllers/main.py
+++ odoo/local-src/website_portal_project/controllers/main.py
@@ -1,1 +1,21 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from openerp import http
+from openerp.addons.website_portal.controllers.main import website_account
+from openerp.http import request
+
+
+class WebsiteAccount(website_account):
+    @http.route(['/my', '/my/home'], type='http', auth="user", website=True)
+    def account(self):
+        response = super(WebsiteAccount, self).account()
+        user = request.env.user
+        projects = request.env['project.project'].search([])
+        response.qcontext.update({'projects': projects})
+        return response
+
+class WebsitePortalProject(http.Controller):
+    @http.route(['/my/projects/<int:project_id>'], type='http', auth="user", website=True)
+    def projects_followup(self, project_id=None):
+        project = request.env['project.project'].browse(project_id)
+        return request.website.render("website_portal_project.projects_followup", {'project': project})
