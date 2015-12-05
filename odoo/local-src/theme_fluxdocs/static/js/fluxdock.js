6e0e003aead4912c2a2a5a74a1d979f1c915cc76
Pierre Sandrin <pierre.sandrin@openinteractive.ch>
2015-12-05 18:12:12 +0100

    Header logo fadein

--- odoo/local-src/theme_fluxdocs/static/js/fluxdock.js
+++ odoo/local-src/theme_fluxdocs/static/js/fluxdock.js
@@ -1,1 +1,16 @@
-console.log('fluxdocs.js is here :-)')
+$(document).ready(function() {
+	
+	console.log('fluxdocs.js is here :-)');
+    
+	
+	$(window).scroll(function(event) {
+		var scrollPosition = $(window).scrollTop();
+		var logo = $('.fluxdock-header-logo');
+		if(scrollPosition > 500){
+			logo.addClass('visible-scroll');
+		}else{
+			logo.removeClass('visible-scroll');
+		}
+    });
+	
+});
