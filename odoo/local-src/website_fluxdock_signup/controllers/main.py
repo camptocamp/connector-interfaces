95985f4298b637a12c37fdc2ead588482067d16b
Goran Sunjka <gs@sunjka.de>
2016-03-01 08:47:29 +0100

    ADD send confirmation email.

--- odoo/local-src/website_fluxdock_signup/controllers/main.py
+++ odoo/local-src/website_fluxdock_signup/controllers/main.py
@@ -7,9 +7,11 @@ from openerp import http
 from openerp.http import request
 from openerp import tools
 from openerp.tools.translate import _
+from openerp import SUPERUSER_ID
 
 from openerp.addons.website_portal.controllers.main import website_account
 from openerp.addons.auth_signup.res_users import SignupError
+from openerp.addons.auth_signup.res_users import res_users
 from openerp.addons.web.controllers.main import ensure_db
 
 
@@ -25,11 +27,14 @@ class AuthSignupHome(openerp.addons.web.controllers.main.Home):
         qcontext = self.get_auth_signup_qcontext()
         response = super(AuthSignupHome, self).web_auth_signup()
         countries = request.env['res.country'].sudo().search([])
+        cr, uid, context = request.cr, request.uid, request.context
+        mail_template = request.registry['mail.template']
         response.qcontext.update({
             'countries': countries,
         })
         if 'error' not in qcontext and request.httprequest.method == 'POST':
             try:
+                mail_template.send_mail(cr, SUPERUSER_ID, 6, 9, force_send=True, raise_exception=True, context=None)
                 return request.render('website_fluxdock_signup.thanks_for_registration', {})
             except (SignupError, AssertionError), e:
                 qcontext['error'] = _(e.message)
