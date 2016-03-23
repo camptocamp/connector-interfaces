923bd00bcd4a71fb6e0c0090e44035f0507d1978
Goran Sunjka <gs@sunjka.de>
2016-03-23 07:07:16 +0100

    FIX problem with saving terms of use.

--- odoo/local-src/website_fluxdock_signup/models/terms_of_use.py
+++ odoo/local-src/website_fluxdock_signup/models/terms_of_use.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import osv, fields
+from openerp.osv import osv
+from openerp import fields, api, models
 
-class TermsOfUse(osv.Model):
-    _name = 'terms.of.use'
-    _inherit = 'res.config.settings'
+class TermsOfUse(models.Model):
+    _name = "terms.of.use"
     _description = 'Terms of use'
 
-    _columns = {
-        'website_id': fields.many2one('website', string="website", required=True),
-        'terms_of_use': fields.text('Terms of use', help='Terms of use', translate=True),
-        'privacy_policy': fields.text('Privacy policy', help='Privacy policy', translate=True),
-    }
+    name = fields.Char(string='Name', size=64, help='Terms of use name')
+    website_id = fields.Many2one(string="Website", required=True)
+    terms_of_use = fields.Text(string='Terms of use', help='Terms of use', translate=True)
+    privacy_policy = fields.Text(string='Privacy policy', help='Privacy policy', translate=True)
 
     def on_change_website_id(self, cr, uid, ids, website_id, context=None):
         if not website_id:
@@ -22,7 +21,6 @@ class TermsOfUse(osv.Model):
                 values[fname] = v[0] if v and self._columns[fname]._type == 'many2one' else v
         return {'value' : values}
 
-
     _defaults = {
-        'website_id': lambda self,cr,uid,c: self.pool.get('website').search(cr, uid, [], context=c)[0],
+        'website_id': lambda self,cr,uid,context: self.pool.get('website').search(cr, uid, [], context=context)[0]
     }
