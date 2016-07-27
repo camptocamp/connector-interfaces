f4745dd93af2643e70ffb1b332771e9ba13e7a4c
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-07-27 16:14:41 +0200

    Setup travis to push docker image on reg.fluxdock.io (still needs to configure access on travis)

---
+++ travis/publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+set -e
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  docker login reg.fluxdock.io --username="$DOCKER_USERNAME" --password="$DOCKER_PASSWORD"
+
+  if [ "$TRAVIS_BRANCH" == "9.0" ]; then
+    echo "Deploying image to docker hub for master (latest)"
+    docker tag fluxdock_odoo odoo/odoo:latest
+    docker push "odoo/odoo:latest"
+  elif [ ! -z "$TRAVIS_TAG" ]; then
+    echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"
+    docker tag fluxdock_odoo odoo/odoo:${TRAVIS_TAG}
+    docker push "odoo/odoo:${TRAVIS_TAG}"
+  elif [ ! -z "$TRAVIS_BRANCH" ]; then
+    echo "Deploying image to docker hub for branch ${TRAVIS_BRANCH}"
+    docker tag fluxdock_odoo odoo/odoo:${TRAVIS_BRANCH}
+    docker push "odoo/odoo:${TRAVIS_BRANCH}"
+  else
+    echo "Not deploying image"
+  fi
+fi
