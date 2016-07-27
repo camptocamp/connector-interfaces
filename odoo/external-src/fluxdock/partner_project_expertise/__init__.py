3412baa928e17c662ac0f76edfefd7194d29fa17
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-07-27 17:07:16 +0200

    Remove fluxdock addons as added in local-src and updated to last version

--- odoo/external-src/fluxdock/partner_project_expertise/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
-# directory
-##############################################################################
-import project
-import res_partner
-import partner_project_expertise
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- odoo/external-src/fluxdock/website_terms_of_use/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import terms_of_use
