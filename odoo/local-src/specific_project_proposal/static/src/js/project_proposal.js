eeef17ec9ace90df5540cd6771bf62ac96cd7cb3
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-08-09 14:37:08 +0200

    Add button to hide a project

---
+++ odoo/local-src/specific_project_proposal/static/src/js/project_proposal.js
@@ -0,0 +1,22 @@
+odoo.define('specific_project_proposal.specific_project_proposal', function (require) {
+'use strict';
+
+var ajax = require('web.ajax');
+var core = require('web.core');
+var website = require('website.website');
+
+var _t = core._t;
+
+if(!$('.oe_website_proposals').length) {
+    return $.Deferred().reject("DOM doesn't contain '.oe_website_proposals'");
+}
+
+    $('.hide_proposal').on('click', function (ev) {
+        ev.preventDefault();
+        var $link = $(ev.currentTarget);
+        ajax.jsonRpc($link.data('href'), 'call', {}).then(function () {
+            $link.parents('.proposal_item').first().remove();
+        });
+    });
+
+});
