e20766f24c56f25c286116664f1ed432843bf221
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-08-09 14:50:32 +0200

    Remove own proposals from proposal matches

--- odoo/local-src/specific_project_proposal/models/user.py
+++ odoo/local-src/specific_project_proposal/models/user.py
@@ -21,14 +21,23 @@ class ResUsers(models.Model):
         compute='_get_suggested_proposals',
         comodel_name='project.proposal',
         string="Suggested proposals",
+        help="Proposals that match with user expertises and industries."
+             " (Blacklisted and own proposals are not part of this list)"
     )
 
     @api.depends('partner_id.category_id', 'partner_id.expertise_ids',
                  'proposal_blacklist_ids')
     def _get_suggested_proposals(self):
+        """ Get list of suggestion of proposals
+
+        Matching proposals based on expertises and industries.
+        It doesn't includ self proposals and proposals marked as blacklisted
+        by the user.
+        """
         self.suggested_proposal_ids = self.env['project.proposal'].search(
             ['|', ('expertise_ids', 'in', self.partner_id.expertise_ids.ids),
                   ('industry_ids', 'in', self.partner_id.category_id.ids),
              ('id', 'not in', self.proposal_blacklist_ids.ids),
+             ('owner_id', '!=', self.id),
              ]
         )
