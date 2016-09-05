209d76b9c124ece44beee934a4578c4008359832
Goran Sunjka <gs@sunjka.de>
2016-09-05 05:26:42 +0200

    Industries and expertises were not loaded behind a ssl proxy in another language than english due to an insecure http call. The problem was fixed by loading industries and expertises with jsonrpc.

--- odoo/local-src/website_portal_profile/static/src/js/website_portal_profile.js
+++ odoo/local-src/website_portal_profile/static/src/js/website_portal_profile.js
@@ -3,22 +3,24 @@ odoo.define('website_portal_profile.portal_profile', function (require) {
 
 var ajax = require('web.ajax');
 var core = require('web.core');
+var Widget = require('web.Widget');
+var base = require('web_editor.base');
 var website = require('website.website');
 
+var qweb = core.qweb;
 var _t = core._t;
+var page_widgets = {};
 
 var lastsearch;
 
-// if(!$('.portal_profile').length) {
-//     return $.Deferred().reject("DOM doesn't contain '.portal_profile'");
-// }
+if(!$('.js_select2_categories').length) {
+    return $.Deferred().reject("DOM doesn't contain '.js_select2_categories'");
+}
 
     $('input.js_select2_categories').select2({
-        tags: true,
+	tags: true,
         tokenSeparators: [",", " ", "_"],
         lastsearch: [],
-	dropdownCssClass: "specific-select2-dropdown",
-	containerCssClass: "specific-select2-container",
         createSearchChoice: function (term) {
             if ($(lastsearch).filter(function () { return this.text.localeCompare(term) === 0;}).length === 0) {
 		return {
@@ -30,31 +32,31 @@ var lastsearch;
         },
         formatResult: function(term) {
             if (term.isNew) {
-                return '<span class="label label-primary specific-select2-results">New</span> ' + _.escape(term.text);
+                return '<span class="label label-primary">New</span> ' + _.escape(term.text);
             }
             else {
                 return _.escape(term.text);
             }
         },
-        ajax: {
-            url: '/my/get_categories',
-            dataType: 'json',
-            data: function(term) {
-                return {
-                    q: term,
-                    l: 50
-                };
-            },
-            results: function(data) {
-                var ret = [];
-                _.each(data, function(x) {
-                    ret.push({ id: x.id, text: x.name, isNew: false });
-                });
-                lastsearch = ret;
-                return { results: ret };
-            }
-        },
-        // Default elements from the input value
+	query: function (query) {
+		ajax.jsonRpc("/web/dataset/call_kw", 'call', {
+			model: 'res.partner.category',
+			method: 'search_read',
+			args: [],
+			kwargs: {
+				fields: ['name'],
+				context: base.get_context()
+			}
+		}).then(function (data) {
+			var tags = { results: [] };
+			_.each(data, function(x) {
+			    tags.results.push({ id: x.id, text: x.name, isNew: false });
+			});
+			lastsearch = tags;
+			query.callback(tags);
+	        });
+	},
+        // Default tags from the input value
         initSelection: function (element, callback) {
             var data = [];
             _.each(element.data('init-value'), function(x) {
@@ -64,6 +66,7 @@ var lastsearch;
             callback(data);
         },
     });
+
     $('input.js_select2_expertises').select2({
         tags: true,
         tokenSeparators: [",", " ", "_"],
@@ -85,24 +88,24 @@ var lastsearch;
                 return _.escape(term.text);
             }
         },
-        ajax: {
-            url: '/my/get_expertises',
-            dataType: 'json',
-            data: function(term) {
-                return {
-                    q: term,
-                    l: 50
-                };
-            },
-            results: function(data) {
-                var ret = [];
-                _.each(data, function(x) {
-                    ret.push({ id: x.id, text: x.name, isNew: false });
-                });
-                lastsearch = ret;
-                return { results: ret };
-            }
-        },
+	query: function (query) {
+		ajax.jsonRpc("/web/dataset/call_kw", 'call', {
+			model: 'partner.project.expertise',
+			method: 'search_read',
+			args: [],
+			kwargs: {
+				fields: ['name'],
+				context: base.get_context()
+			}
+		}).then(function (data) {
+			var tags = { results: [] };
+			_.each(data, function(x) {
+			    tags.results.push({ id: x.id, text: x.name, isNew: false });
+			});
+			lastsearch = tags;
+			query.callback(tags);
+	        });
+	},
         // Default tags from the input value
         initSelection: function (element, callback) {
             var data = [];
