323094a764cf16b9b67cdadd79a7b9ebd216f4b0
Simone Orsi <simahawk@gmail.com>
2016-10-21 17:05:47 +0200

    fix broken upgrade

--- odoo/local-src/website_fluxdock_signup/__openerp__.py
+++ odoo/local-src/website_fluxdock_signup/__openerp__.py
@@ -9,7 +9,12 @@
     'category': 'Website',
     'version': '1.0',
     'depends': [
-        'base',
+        # TODO 2016-10-21: dep needed to make the upgrade work.
+        # Without this all the fields previously defined by this module
+        # are not found and all related inherited views are broken.
+        # Remove it (or the whole module)
+        # after the 1st upgrade of `specific_membership`
+        'specific_membership',
     ],
     'data': [
     ],
--- odoo/local-src/website_portal_profile/__openerp__.py
+++ odoo/local-src/website_portal_profile/__openerp__.py
@@ -9,7 +9,12 @@
         """,
     'website': 'https://www.sunjka.de/',
     'depends': [
-        'base',
+        # TODO 2016-10-21: dep needed to make the upgrade work.
+        # Without this all the fields previously defined by this module
+        # are not found and all related inherited views are broken.
+        # Remove it (or the whole module)
+        # after the 1st upgrade of `specific_membership`
+        'specific_membership',
     ],
     'data': [
     ],
