abb20f1074c88d771a6f6dc44d9af873e40e22af
Simone Orsi <simahawk@gmail.com>
2016-10-25 08:45:41 +0200

    [imp] update login if email is validated and publish partner

--- odoo/local-src/specific_membership/controllers/account.py
+++ odoo/local-src/specific_membership/controllers/account.py
@@ -2,12 +2,18 @@
 # © 2016 Denis Leemann (Camptocamp)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import http, tools, _
+from openerp import http, _
 from openerp.http import request
 from openerp.addons.website_portal.controllers.main import website_account
 
 import json
 import base64
+import logging
+_logger = logging.getLogger(__name__)
+try:
+    from validate_email import validate_email
+except ImportError:
+    _logger.debug("Cannot import `validate_email`.")
 
 
 class WebsiteAccount(website_account):
@@ -27,7 +33,8 @@ class WebsiteAccount(website_account):
     @http.route(['/my/account'], type='http', auth="user", website=True)
     def details(self, redirect=None, **post):
         vals = {}
-        partner = request.env['res.users'].browse(request.uid).partner_id
+        user = request.env['res.users'].browse(request.uid)
+        partner = user.partner_id
         if redirect:
             redirect = redirect
         else:
@@ -57,9 +64,17 @@ class WebsiteAccount(website_account):
                 vals['expertise_ids'] = [(6, None, expertise_ids)]
             if post['uimage']:
                 vals['image'] = base64.encodestring(post['uimage'].read())
+
+            vals['email_updated'] = self._handle_email_update(user, post)
+            # TODO: show a message to the user
+            # saying that the login has changed
             response.qcontext.update(vals)
+
             if 'error' not in response.qcontext:
                 vals['website'] = post['website_url']
+                # finally publish the partner
+                if not partner.website_published:
+                    vals['website_published'] = True
                 partner.sudo().write(vals)
 
         if industry_ids:
@@ -80,6 +95,17 @@ class WebsiteAccount(website_account):
 
         response.qcontext.update(categories=industries, expertises=expertises)
         return response
+
+    def _handle_email_update(self, user, post):
+        """Validate email update and handle login update."""
+        if user.email != post.get('email'):
+            email = post['email']
+            valid = validate_email(email)
+            if email and valid:
+                # update login on user
+                user.sudo().write({'login': email})
+                return True
+        return False
 
     def details_form_validate(self, data):
         """ Overwrite checks """
@@ -105,8 +131,7 @@ class WebsiteAccount(website_account):
             error_message.append(_('Some required fields are empty.'))
 
         # email validation
-        if data.get('email') and not tools.single_email_re.match(
-                data.get('email')):
+        if data.get('email') and not validate_email(data.get('email')):
             error["email"] = 'error'
             error_message.append(
                 _('Invalid Email! Please enter a valid email address.'))
--- odoo/local-src/specific_membership/models/res_users.py
+++ odoo/local-src/specific_membership/models/res_users.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from openerp import models
+from openerp import _
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
     _sql_constraints = [
-        ('login',
+        (
+            'login',
             'UNIQUE (login)',
-            'A user with this login already exists !')]
+            _('A user with this login already exists !')
+        )
+    ]
