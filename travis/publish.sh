ace178f167d97bab23b4a10c5ba832ed18a41b41
Denis Leemann <denis.leemann@camptocamp.com>
2016-10-26 09:54:22 +0200

    update travis configuration

--- travis/publish.sh
+++ travis/publish.sh
@@ -8,16 +8,19 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Deploying image to docker hub for master (latest)"
     docker tag fluxdock_odoo camptocamp/fluxdock_odoo:latest
-    docker push "reg.fluxdock.io:5000/odoo/odoo:latest"
+    docker push "camptocamp/fluxdock_odoo:latest"
   elif [ ! -z "$TRAVIS_TAG" ]; then
     echo "Deploying image to docker hub for tag ${TRAVIS_TAG}"
     docker tag fluxdock_odoo camptocamp/fluxdock_odoo:${TRAVIS_TAG}
-    docker push "reg.fluxdock.io:5000/odoo/odoo:${TRAVIS_TAG}"
+    docker push "camptocamp/fluxdock_odoo:${TRAVIS_TAG}"
   elif [ ! -z "$TRAVIS_BRANCH" ]; then
     echo "Deploying image to docker hub for branch ${TRAVIS_BRANCH}"
     docker tag fluxdock_odoo camptocamp/fluxdock_odoo:${TRAVIS_BRANCH}
-    docker push "reg.fluxdock.io:5000/odoo/odoo:${TRAVIS_BRANCH}"
+    docker push "camptocamp/fluxdock_odoo:${TRAVIS_BRANCH}"
   else
     echo "Not deploying image"
   fi
+
+  echo "This is a PR"
+
 fi
