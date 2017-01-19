9036ae0d66ea40f36f2f0ea120d9edc42cb5495e
Simone Orsi <simahawk@gmail.com>
2017-01-19 12:34:30 +0100

    [fix] former bad rebase/merge

--- odoo/songs/install/pre.py
+++ odoo/songs/install/pre.py
@@ -9,18 +9,12 @@ import os
 @anthem.log
 def setup_language(ctx):
     """ Installing language and configuring locale formatting """
-    website_langs = [ctx.env.ref('base.lang_en').id, ]
     for code in ('de_DE',):
         ctx.env['base.language.install'].create({'lang': code}).lang_install()
-        lang = ctx.env['res.lang'].search([('code', '=', code)])
-        website_langs.append(lang.id)
     ctx.env['res.lang'].search([]).write({
         'grouping': [3, 0],
         'date_format': '%d/%m/%Y',
     })
-    ws = ctx.env['website'].browse(1)
-    ws.write(
-        {'language_ids': [(6, 0, website_langs), ]})
 
 
 @anthem.log
@@ -38,6 +32,8 @@ def setup_url_params(ctx):
     # make pdf report generation happy
     # see https://github.com/odoo/odoo/issues/1105
     # TMP fix: we need to move this stuff to environment files
+    # but currently you cannot use env vars in env files
+    # because env vars are not loaded :(
     # PR for odoo project
     # https://github.com/camptocamp/docker-odoo-project/pull/20
     url = os.environ.get('DOMAIN_NAME', 'http://localhost:8069')
