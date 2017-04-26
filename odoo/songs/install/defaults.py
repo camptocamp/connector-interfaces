4eb3497131347d0424a1330a2c52fab34048222b
Simone Orsi <simahawk@gmail.com>
2017-04-26 14:05:23 +0200

    [fix] apply defaults to user template too
    When creating user trough backend interface
    having defaults defined at field level is enough.
    But when creating users via website interface
    most of the defaults for involved fields
    are taken from the user template by `auth_signup` module.

--- odoo/songs/install/defaults.py
+++ odoo/songs/install/defaults.py
@@ -9,9 +9,18 @@ DEFAULTS = (
     ('res.partner', 'notify_email', 'digest', ),
     ('res.partner', 'notify_frequency', 'weekly', ),
 )
+USER_TEMPLATE_DEFAULTS = (
+    # set defaults from here as this template user
+    # has `noupdate=1` hence we cannot force it via xml.
+    ('auth_signup.default_template_user', 'notify_email', 'digest', ),
+    ('auth_signup.default_template_user', 'notify_frequency', 'weekly', ),
+)
 
 
 @anthem.log
 def default_values(ctx):
     company = ctx.env.ref('base.main_company')
     set_default_values(ctx, company, DEFAULTS)
+
+    for xmlid, fname, value in USER_TEMPLATE_DEFAULTS:
+        ctx.env.ref(xmlid).write({fname: value})
