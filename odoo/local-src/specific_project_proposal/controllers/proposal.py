4ee4221d3a4e371fbb5541dd4fce876862257c91
Simone Orsi <simahawk@gmail.com>
2017-05-18 17:43:55 +0200

    be defensive when proposals gets unpuslished #17172

--- odoo/local-src/specific_project_proposal/controllers/proposal.py
+++ odoo/local-src/specific_project_proposal/controllers/proposal.py
@@ -11,6 +11,14 @@ class Proposal(http.Controller):
     @http.route('/market/proposals/<model("project.proposal"):proposal>',
                 type='http', auth="public", website=True)
     def proposals_detail(self, proposal, filters='all', **kwargs):
+        try:
+            proposal.check_access_rights('read')
+            proposal.check_access_rule('read')
+            redirect = ''
+        except:
+            redirect = '/market'
+        if redirect:
+            return request.redirect(redirect)
         return request.render("specific_project_proposal.proposal_detail", {
             'proposal': proposal,
             'main_object': proposal,
