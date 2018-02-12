2aff803c52d758e1507871b7903196a94cb31639
Simone Orsi <simahawk@gmail.com>
2018-02-12 12:34:26 +0100

    Theme: add new 2 cols layout BSFLU-59

---
+++ odoo/local-src/fluxdock_theme/controllers/design_helpers.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import http
+from odoo.http import request
+
+
+class DesignHelpers(http.Controller):
+    """Views for visual componentss."""
+
+    @http.route(
+        ['/flux-design/2-cols'],
+        type='http', auth="public", website=True)
+    def layout_2_cols(self, **post):
+        vals = {
+            'section_logo':
+                '/fluxdock_theme/static/img/content-icons/collaborative.png',
+            'section_title': 'Test section',
+        }
+        return request.render("fluxdock_theme.test_2cols_layout", vals)
