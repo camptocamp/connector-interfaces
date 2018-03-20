c6bd2c45ee89496c0834eacc4097a79b7b2eb0b0
Simone Orsi <simahawk@gmail.com>
2018-03-20 08:04:55 +0100

    Fix ir.http anon session: restore fix for v9

--- odoo/local-src/fluxdock_theme/models/ir_http.py
+++ odoo/local-src/fluxdock_theme/models/ir_http.py
@@ -1,24 +1,23 @@
 # Copyright 2016 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-# TODO: this was for v9. Check if still needed on v11
-# https://github.com/OCA/web/pull/402
 """
 TMP workaround for anon sessions
 https://github.com/laslabs/web/blob/f8cfa10c4f594dda5a424c0e580e9b5b9d361e57
 /web_session_allow_public/models/ir_http.py
 """
-#
-# from odoo import models
-# from odoo.http import SessionExpiredException
+
+from odoo import models
+from odoo.http import SessionExpiredException
 
 
-# class IrHttp(models.AbstractModel):
-#
-#     _inherit = 'ir.http'
-#
-#     def _auth_method_user(self):
-#         try:
-#             return super(IrHttp, self)._auth_method_user()
-#         except SessionExpiredException:
-#             return self._auth_method_public()
+class IrHttp(models.AbstractModel):
+
+    _inherit = 'ir.http'
+
+    @classmethod
+    def _auth_method_user(cls):
+        try:
+            return super()._auth_method_user()
+        except SessionExpiredException:
+            return cls._auth_method_public()
